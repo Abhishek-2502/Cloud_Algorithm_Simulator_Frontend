b1431a46e18cb72ac491a02b5941cdb9aae77dcc
Abhishek-2502 <abhishek25022004@gmail.com>
2025-03-30 13:24:08 +0530

    Added ec2 url

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def simulate():
         }
 
         logging.info("Sending request to Spring Boot API")
-        response = requests.post('http://localhost:8080/api/simulation/run', json=data)
+        response = requests.post('http://65.0.181.53:8080/api/simulation/run', json=data)
 
         if response.status_code != 200:
             logging.error(f"Spring Boot API Error: {response.status_code} - {response.text}")
